ab2b2e27d8955225308d249ec19353162516756d
Anton Skuratov <skan202120@gmail.com>
2021-12-10 15:19:48 +0300

    feat: change stepper to custom buttons

--- Your_Choice/Controller /PlayersViewController/PlayersViewController.swift
+++ Your_Choice/Controller /PlayersViewController/PlayersViewController.swift
@@ -7,28 +7,16 @@
 import UIKit
 
 class PlayersViewController: BaseViewController {
-    
+    //MARK:- Properties
     var playerName = [String?]()
     var index = 0
-    var rowsCounts: Int = {
+    var playersCounts: Int = {
             return 2
     }()
     var mainPlayerName: String {
       return UserDefaults.standard.value(forKey: "PlayersName") as! String
     }
-    private var stepper: UIStepper = {
-        var step = UIStepper()
-        step.translatesAutoresizingMaskIntoConstraints = false
-        step.autorepeat = true
-        step.maximumValue = 5
-        step.minimumValue = 0
-        step.stepValue = 1
-        step.value = 2
-        step.wraps = false
-        step.addTarget(self, action: #selector(stepAction), for: .valueChanged)
-        
-        return step
-    }()
+    //MARK:- UI
 private var tableView: UITableView = {
         var tableView = UITableView()
         tableView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
@@ -72,7 +60,47 @@ private var tableView: UITableView = {
         
    }()
     
+    private var plusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "plus.rectangle"), for: .normal)
+        button.layer.borderWidth = 2
+        button.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.backgroundColor = #colorLiteral(red: 0.5555383563, green: 0, blue: 1, alpha: 1)
+        button.tintColor = .white
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(plusPlayer), for: .touchUpInside)
+        return button
+        
+   }()
+    private var minusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "minus.rectangle"), for: .normal)
+        button.largeContentImage = .add
+        
+        button.layer.borderWidth = 2
+        button.layer.borderColor = #colorLiteral(red: 0.5555383563, green: 0, blue: 1, alpha: 1)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        button.tintColor = #colorLiteral(red: 0.5555383563, green: 0, blue: 1, alpha: 1)
+        button.layer.cornerRadius = 25
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(minusPlayer), for: .touchUpInside)
+        return button
+        
+   }()
     
+    private var buttonView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .none
+        return view
+    }()
+    
+    //MARK:- life cycle VC
     override func viewDidLoad() {
         super.viewDidLoad()
         addElementsToView()
@@ -96,40 +124,73 @@ private var tableView: UITableView = {
         nextButton.widthAnchor.constraint(equalToConstant: 250).isActive = true
         nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+
+        minusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        minusButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        minusButton.leftAnchor.constraint(equalTo: buttonView.leftAnchor  ).isActive = true
+        minusButton.bottomAnchor.constraint(equalTo: buttonView.bottomAnchor).isActive = true
         
+        plusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        plusButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusButton.rightAnchor.constraint(equalTo: buttonView.rightAnchor  ).isActive = true
+        plusButton.bottomAnchor.constraint(equalTo: buttonView.bottomAnchor).isActive = true
+
         tableView.topAnchor.constraint(equalTo: mainTextField.bottomAnchor, constant: 40).isActive = true
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30).isActive = true
         tableView.heightAnchor.constraint(equalToConstant: 220).isActive = true
+        
+        buttonView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        buttonView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        buttonView.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -10).isActive = true
+        buttonView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+       
     
-        stepper.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        stepper.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -20).isActive = true
+
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
     }
+    //MARK:- objc metods
     
-
-    @objc private func stepAction(_ sender: UIStepper) {
+    @objc private func plusPlayer() {
+       let maxPlayers = 5
+      
+        if playersCounts < maxPlayers {
+            playersCounts += 1
+            tableView.reloadData()
+        }else {
+             print(playersCounts) // добавить лейбл с оповещением
+        }
+      
+    }
+    
+    @objc private func minusPlayer() {
+        let minPlayers = 0
+        if playersCounts > minPlayers {
+            playersCounts -= 1
+            tableView.reloadData()
+        }else {
+            print(playersCounts) // добавить лейбл с оповещением
+        }
        
-        let value =  Int(stepper.value)
-        rowsCounts = value
-        tableView.reloadData()
-
     }
     
     @objc private func backAction() {
         navigationController?.popViewController(animated: true)
     }
     
+    //MARK:- metods
     private func addElementsToView() {
         view.addSubview(mainTextField)
         view.addSubview(nextButton)
         view.addSubview(tableView)
-        view.addSubview(stepper)
-      
+        view.addSubview(buttonView)
+    
+        buttonView.addSubview(minusButton)
+        buttonView.addSubview(plusButton)
     }
     
     private func addDelegate() {
@@ -147,8 +208,8 @@ private var tableView: UITableView = {
         playerName.append("Игрок")
     }
     
-   
 }
+//MARK:- extensions textFields
 extension PlayersViewController: UITextFieldDelegate {
     
   func textFieldDidEndEditing(_ textField: UITextField) {
@@ -174,6 +235,7 @@ extension PlayersViewController: UITextFieldDelegate {
     }
     
 }
+//MARK:- extensions TableView
 extension PlayersViewController: UITableViewDelegate, UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -181,7 +243,7 @@ extension PlayersViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rowsCounts
+        return playersCounts
     }
     
     
